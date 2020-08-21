b20f36d990e144fac19f91556a93b6a3fa6a7b69
FW-workspace <erikii2019@outlook.com>
2020-08-21 22:14:40 +0300

    ud

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,2 +1,5 @@
-import random
-listchar=[]
+import string
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(8, 16)))
+print password
